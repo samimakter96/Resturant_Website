0e6636ab9f85f00550685b145fd4d288cda2fdb7
samimakter96 <samimakter9641@gmail.com>
2024-04-27 09:34:08 +0530

    Implementing the cart Itemns page

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,22 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CartContext from "./cart-context";
 
 
 const CartProvider = (props) => {
-
-  const [items, setItems] = useState([])
+  const [items, setItems] = useState([]);
 
   const addItemToCartHandler = (item) => {
-    setItems([...items,item]);
+    setItems([...items, item]);
   };
 
   const removeItemFromCartHandler = (id) => {};
 
+  // Calculate the total amount whenever items change
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  useEffect(() => {
+    const total = items.reduce((accumulator, item) => {
+      return accumulator + item.price; // Assuming each item has a 'price' property
+    }, 0);
+
+    // Format total amount to two decimal places
+    setTotalAmount(parseFloat(total.toFixed(2)));
+  }, [items]);
+
   const cartContext = {
     items: items,
+    totalAmount: totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
-  }
+  };
 
   return (
     <CartContext.Provider value={cartContext}>
